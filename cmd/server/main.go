a800f32f5551add1c97c248dc071e3b76526ef3f
JerryDdie77 <shahterchannel@gmail.com>
2026-01-25 19:23:34 +0300

    creating a start point in project, updating gitignore

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"jerry-notes/internal/api"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	// Создаём роутер и регистрируем все хендлеры в нём
+	// Getting environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Can not Load .env variables", err)
+	}
+
+	connStr := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("sql Open: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("db Ping: %v", err)
+	}
+
+	// Creating the route and register all handlers in them
 	r := mux.NewRouter()
 	api.RegisterRoutes(r)
 
-	// Запускаем сервер на порту :8080
+	// Stating a server in a port :8080
 	http.ListenAndServe(":8080", r)
 }
