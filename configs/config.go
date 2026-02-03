d97ae7c5c333dc3fd8d966a5272d30ea7c281f6c
JerryDdie77 <shahterchannel@gmail.com>
2026-02-03 11:12:50 +0300

    feat: making separate directory for config

---
+++ configs/config.go
@@ -0,0 +1,34 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	JWTSecret string
+	DBURL     string
+}
+
+func LoadConfig() (*Config, error) {
+
+	if err := godotenv.Load(); err != nil {
+		return &Config{}, fmt.Errorf("load: %w", err)
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+
+	if secret == "" {
+		return &Config{}, fmt.Errorf("JWT_SECRET is required")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+
+	if dbURL == "" {
+		return &Config{}, fmt.Errorf("DB_URL is required")
+	}
+
+	return &Config{JWTSecret: secret, DBURL: dbURL}, nil
+}
