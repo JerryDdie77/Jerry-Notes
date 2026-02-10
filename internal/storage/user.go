3bce940ab17d5ebf1ca24e9a9231098171f59d1e
JerryDdie77 <shahterchannel@gmail.com>
2026-02-10 11:09:30 +0300

    feat: implementing the UserStorage interface

--- internal/storage/user.go
+++ internal/storage/user.go
@@ -18,7 +18,7 @@ func NewPostgresUserStorage(db *sql.DB) *PostgresUserStorage {
 	}
 }
 
-func (p PosgtresNoteStorage) GetUser(ctx context.Context, email string) (service.User, error) {
+func (p PosgtresNoteStorage) GetUserByEmail(ctx context.Context, email string) (service.User, error) {
 	query := "SELECT id, user_name, created_at, password_hash, email, is_blocked FROM users WHERE email = $1"
 
 	var u service.User
@@ -37,6 +37,21 @@ func (p PosgtresNoteStorage) GetUser(ctx context.Context, email string) (service
 		}
 
 		return service.User{}, fmt.Errorf("scan: %w", err)
+	}
+
+	return u, nil
+}
+
+func (p PosgtresNoteStorage) GetUserByID(ctx context.Context, id int64) (service.User, error) {
+	query := "SELECT id, user_name, created_at, password_hash, email, is_blocked FROM users WHERE id = $1"
+	var u service.User
+	err := p.db.QueryRowContext(ctx, query, id).Scan(&u.ID, &u.Name, &u.CreatedAt, &u.PasswordHash, &u.Email, &u.IsBlocked)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return service.User{}, service.ErrNotFound
+		}
+
+		return service.User{}, fmt.Errorf("Scan: %w", err)
 	}
 
 	return u, nil
@@ -72,4 +87,99 @@ func (p *PostgresUserStorage) CreateUser(ctx context.Context, name, passwordHash
 	}
 
 	return id, nil
+}
+
+func (p *PosgtresUserStorage) SavePendingUser(ctx context.Context, name, passwordHash, email, code string) error {
+	query := "INSERT INTO pending_users(email, user_name, password_hash, code) VALUES($1, $2, $3, $4)"
+
+	res, err := p.db.ExecContext(ctx, query, email, name, passwordHash, code)
+	if err != nil {
+		return fmt.Errorf("ExecContext: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("RowsAffected: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return service.ErrNotFound
+	}
+
+	return nil
+}
+
+func (p PostgresUserStorage) GetPendingUser(ctx context.Context, email string) (service.PendingUser, error) {
+	query := "SELECT email, user_name, password_hash, code, created_at FROM pending_users WHERE email = $1"
+
+	var u service.PendingUser
+
+	row := p.db.QueryRowContext(ctx, query, email)
+	err := row.Scan(&u.Email, &u.Name, &u.PasswordHash, &u.Code, &u.CreatedAt)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return service.PendingUser{}, service.ErrNotFound
+		}
+
+		return service.PendingUser{}, fmt.Errorf("scan: %w", err)
+	}
+
+	return u, nil
+}
+
+func (p *PostgresUserStorage) DeletePendingUser(ctx context.Context, email string) error {
+	query := "DELETE FROM pending_users WHERE email = $1"
+	res, err := p.db.ExecContext(ctx, query, email)
+	if err != nil {
+		return fmt.Errorf("ExecContext: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("RowsAffected: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return service.ErrNotFound
+	}
+
+	return nil
+}
+
+func (p *PostgresUserStorage) ChangeNameByID(ctx context.Context, id int64, newName string) error {
+	query := "UPDATE users SET user_name = $1 WHERE id = $2"
+	res, err := p.db.ExecContext(ctx, query, newName, id)
+	if err != nil {
+		return fmt.Errorf("ExecContext: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("RowsAffected: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return service.ErrNotFound
+	}
+
+	return nil
+}
+
+func (p *PostgresUserStorage) DeleteUserByID(ctx context.Context, id int64) error {
+	query := "DELETE FROM users WHERE id = $1"
+	res, err := p.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("ExecContext: %w", err)
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("RowsAffected: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return service.ErrNotFound
+	}
+
+	return nil
 }
