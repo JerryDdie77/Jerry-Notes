d0412487a3104c304b992a0b57b21712e3c1f3d3
JerryDdie77 <shahterchannel@gmail.com>
2026-02-14 21:11:55 +0300

    feat: JWT manager now is separate service

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,17 +1,22 @@
 package app
 
-import "jerry-notes/internal/service"
+import (
+	"jerry-notes/internal/jwt"
+	"jerry-notes/internal/service"
+)
 
 type App struct {
 	NoteService *service.NoteService
 	UserService *service.UserService
 	AuthService *service.AuthService
+	JWTManager  *jwt.Manager
 }
 
-func New(noteService *service.NoteService, userService *service.UserService, authService *service.AuthService) *App {
+func New(noteService *service.NoteService, userService *service.UserService, authService *service.AuthService, jwtManager *jwt.Manager) *App {
 	return &App{
 		NoteService: noteService,
 		UserService: userService,
 		AuthService: authService,
+		JWTManager:  jwtManager,
 	}
 }
